16a4f5a5a3f7df007f4f3cd4cb0fe2c4cb987892
Kevin <dbsgur46894689@gmail.com>
2022-02-22 22:54:25 +0900

    react-redux-practice

---
+++ src/Routes/Detail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+  const myId = useParams().id;
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+
+  console.log(toDo);
+
+  return (
+    <>
+      {toDo?.text}
+      Created at: {toDo?.id}
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
